324361d501cc6730c3ce68bf9bc143dd692961b4
orangeola <orangeqmm@gmail.com>
2022-07-11 20:48:29 +0930

    Add all files, add ability to add default book to an array and display title on page, add button that activates an add book pop up (non functional)

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.getElementById("container");
 const addButton = document.getElementById("test");
+const bookContainer = document.getElementById('newBookContainer');
 
 let myLibrary = [];
 
@@ -21,10 +22,34 @@ function display() {
     for(let i = 0; i < myLibrary.length; i++) {
         let newDiv = document.createElement("div");
         let p = document.createElement("p");
+        let but = document.createElement("button");
         p.innerText = myLibrary[i].title;
         newDiv.appendChild(p);
+        newDiv.appendChild(but);
         container.appendChild(newDiv);  
     }
 }
 
-addButton.addEventListener('click', addBookToLibrary);
+document.addEventListener('click', function handleClickOutsideBox(event) {
+    const box = document.getElementById('newBook');
+    const boxContainer = document.getElementById('newBookContainer');
+  
+    if (!box.contains(event.target)) {
+        hide();
+    }
+    if(event.target === addButton){
+        boxContainer.style.display = 'flex';
+   }
+  });
+
+function hide()
+{
+    const boxContainer = document.getElementById('newBookContainer');
+    boxContainer.style.display = 'none';
+    document.forms['bookForm'].reset()
+}
+
+addButton.addEventListener('click', () => {
+    const boxContainer = document.getElementById('newBookContainer');
+    boxContainer.style.display = 'flex';
+});
